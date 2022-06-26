bd06edb0b3ec09b89c39b516558fafea1a21660c
ridoy113 <almamounhridoy113@gmail.com>
2022-06-27 00:36:05 +0600

    last

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import logo2 from '../../../img/images/logo2.png'
-import './Header.css'
 import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
             <Container>
                 <Navbar.Brand as={Link} to="/">
-                    <img style={{ width: '160px' }} src={logo2} alt="" />
+                    <img style={{ width: '150px' }} src={logo2} alt="" />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-
+                       
                     </Nav>
                     <Nav>
-                        <Nav.Link as={Link} to="/details">About</Nav.Link>
-                        <Nav.Link as={Link} to="/login">
+                        <Nav.Link className='nav__text' as={Link} to="/details">About</Nav.Link>
+                        <Nav.Link className='nav__text' as={Link} to="/login">
                             LogIN
                         </Nav.Link>
                     </Nav>
